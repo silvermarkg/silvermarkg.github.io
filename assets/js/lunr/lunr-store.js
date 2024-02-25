1ca3cecd444e07f97768e9623bec1964636d5927
silvermarkg <19527097+silvermarkg@users.noreply.github.com>
2024-02-25 23:16:05 +0000

    Updated blog to version 0.0.4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Example Post 1",
+        "excerpt":"This is an example post to see what it looks like. New paragraph not included in exceprt. Lets put lots of text in here to see how it affects differenct layouts. We should also put in an image or two and may typograph elements so I can see what they...","categories": ["Intune","PowerShell"],
+        "tags": ["Intune","Logic app","PowerShell"],
+        "url": "/intune/powershell/example-post-1/",
+        "teaser": null
+      },{
+        "title": "Example Post 2",
+        "excerpt":"This is an example post to see what it looks like. New paragraph not included in exceprt. Lets put lots of text in here to see how it affects differenct layouts. We should also put in an image or two and may typograph elements so I can see what they...","categories": [],
+        "tags": [],
+        "url": "/example-post-2/",
+        "teaser": null
+      }]
--- newfile.md
+++
@@ -1,1 +0,0 @@
-new file chamge change
