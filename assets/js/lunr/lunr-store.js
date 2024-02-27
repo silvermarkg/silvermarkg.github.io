706b433ee5c98d1e4404230c92225e25704dce22
silvermarkg <19527097+silvermarkg@users.noreply.github.com>
2024-02-27 09:09:54 +0000

    Updated blog to version 0.0.5

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,8 +1,8 @@
 var store = [{
         "title": "Example Post 1",
-        "excerpt":"This is an example post to see what it looks like. New paragraph not included in exceprt. Lets put lots of text in here to see how it affects differenct layouts. We should also put in an image or two and may typograph elements so I can see what they...","categories": ["Intune","PowerShell"],
+        "excerpt":"This is an example post to see what it looks like. New paragraph not included in exceprt. Lets put lots of text in here to see how it affects differenct layouts. We should also put in an image or two and may typograph elements so I can see what they...","categories": [],
         "tags": ["Intune","Logic app","PowerShell"],
-        "url": "/intune/powershell/example-post-1/",
+        "url": "/example-post-1/",
         "teaser": null
       },{
         "title": "Example Post 2",
